58b760f8c89f678d84a95579376214caff044257
Gabriel Benevento <gaby88akd@gmail.com>
2023-07-31 11:30:16 -0300

    probando

--- Liliana GameStore/client/src/App.js
+++ Liliana GameStore/client/src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
 
-
 function App() {
-
-
-  return (
-
-    <div className="App">
-      <h1>Hola Guachos.</h1>
-    </div>
-    
-  );
+	return (
+		<div className='App'>
+			<h1>Hola Guachos.</h1>
+		</div>
+	);
 }
 
 export default App;
