41d709fda9bbb31f8c12016ce63bef7c506a7a2a
Galbfran <francogalbiati984@gmail.com>
2023-07-31 11:13:57 -0300

    cambios

---
+++ Liliana GameStore/api/src/routes/index.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+router.post('/',);
+
+router.get('/',);
+
+router.put('/',);
+
+router.delete('/',);
+
+
+
+module.exports = router;
