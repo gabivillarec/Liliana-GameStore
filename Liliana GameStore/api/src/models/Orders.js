674c29f7b3c0d9e1a33e7775ab0f692805410983
Dave <doble.aguila@hotmail.com>
2023-07-31 16:44:33 -0300

    Dave:(models/Cart,Favorites,Orders) Creados los 3 modelos mencionados.

---
+++ Liliana GameStore/api/src/models/Orders.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('orders', {
+        order_numer: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        order_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        total_price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }, { timestamps: false});
+};
