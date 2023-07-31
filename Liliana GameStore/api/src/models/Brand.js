31711f0062b8591b97e3b49607f7f1d37f4bf99f
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-07-31 16:29:07 -0300

    creé modelo brand - gabi villa

---
+++ Liliana GameStore/api/src/models/Brand.js
@@ -0,0 +1,22 @@
+const {DataTypes} = require('sequelize');
+
+const Brand = (Sequelize) => {
+
+    Sequelize.define("brand", {
+        
+        id : {
+            type : DataTypes.INTEGER,
+            autoIncrement : true,
+            primaryKey : true,
+            allowNull : false,
+        },
+
+        name : {
+            type: DataTypes.STRING,
+            allowNull : false,
+        }
+
+    },{timestamps : false});
+};
+
+module.export = Brand;
