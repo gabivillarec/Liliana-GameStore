498f25b4f9a861cc728d2946f3b460baa5f8cbc7
Gabriel Benevento <gaby88akd@gmail.com>
2023-07-31 16:09:25 -0300

    model product

---
+++ Liliana GameStore/api/src/models/Products.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+
+const Products = (sequelize) => {
+	sequelize.define('products', {
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		price: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		image: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		stock: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	});
+};
+
+module.exports = Products;
