c7daf319b9206820a8134b46eab1217aae88db7b
diego insaurralde <djinsaurralde38@gmail.com>
2023-08-01 10:35:39 -0300

    array prueba Diego

--- Liliana GameStore/client/src/components/Inicio/Inicio.jsx
+++ Liliana GameStore/client/src/components/Inicio/Inicio.jsx
@@ -1,10 +1,10 @@
 //hooks
 import Carousel from "./Carousel/Carousel";
 import CardsContainer from "../CardsContainer/CardsContainer";
-import {useSelector} from './react-redux'
+import {useSelector} from 'react-redux'
 
 const Inicio = () => {
-    const {products } = useSelector()
+    const {products } = useSelector(state => state)
 
     return (
         <div>
