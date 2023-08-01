3d4090cc1b1f780ecb9fc49680d5b277b987db86
Villalobos Gabriel Andres <71297222+gabivillarec@users.noreply.github.com>
2023-08-01 11:24:10 -0300

    Create index.js

---
+++ Liliana GameStore/api/src/routes/index.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const usersRoutes = require('./usersRoutes')
+
+const router = Router();
+
+// Configurar los routers
+
+router.use('/LilianaGameStore',usersRoutes);
+//router.use('/LilianaGameStore',productRoutes);
+//router.use('/LilianaGameStore',brandRoutes);
+//router.use('/LilianaGameStore',cartRoutes);
+//router.use('/LilianaGameStore',categoryRoutes);
+//router.use('/LilianaGameStore',favoritesRoutes);
+//router.use('/LilianaGameStore',orderRoutes);
+//router.use('/LilianaGameStore',purchaseHistoryRoutes);
+//router.use('/LilianaGameStore',socketRoutes);
+//router.use('/LilianaGameStore',subCategoryRoutes);
+
+
+module.exports = router;
