7a3fb02ad1c77b943e22bcd3cc1360618e1dc0c6
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-08-01 11:02:10 -0300

    gabiV:(api/src/controllers & routes) creé el controller createUser.js y lo importe dentro de la ruta userRoutes.js / tambien creé y deje comentado las rutas que vamos a usar en el index.js

---
+++ Liliana GameStore/api/src/Controllers/createUser.js
@@ -0,0 +1,41 @@
+const {User} = require ("../db");
+
+const createUser = async (req,res)=>{
+
+    try {
+
+        const {name,username,email,password,cp,address,phone,avatar_img,admin} = req.body
+
+        if(username && email){
+
+            const [register, created] = await User.findOrCreate({
+                where : {username , email},
+                default: {name,password,cp,address,phone,avatar_img,admin}
+            });
+
+            if(created){
+
+                const response = {
+                    name: register.name,
+                    username: register.username,
+                    email: register.email,
+                    password: register.password,
+                    cp: register.cp,
+                    address: register.address,
+                    phone: register.phone,
+                    avatar_img,admin: register.avatar_img,
+                }
+
+                return res.json(response)
+            } else {
+                return res.status(400).send("The user already exists, try another one")
+            }
+        };
+        return res.status(400).send("email and username are require")
+    } catch (error) {
+        return res.status(500).json({error: error.message})
+    };
+
+};
+
+module.exports = createUser;
