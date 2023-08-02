a9ea415133d42fda9234f4622c56ed48640f9e96
Nicolas Rojas <nico_rojas09@hotmail.com>
2023-08-02 19:43:20 -0300

    navbar actualizada

--- Liliana GameStore/client/src/components/Nav/NavBar.jsx
+++ Liliana GameStore/client/src/components/Nav/NavBar.jsx
@@ -8,11 +8,21 @@ const NavBar = () => {
         <nav id="navbar-example2" className={`navbar px-3 p-3 ${style.navbarE}`}>
             <img className={style.logo} src='../../../vistas/LilianaGameStore.png' alt="logo" onClick={() => navigate('/')}/>
             <div className="d-flex flex-wrap gap-2">
-                <button className="btn btn-outline-info border-3 fs-5" onClick={()=> navigate('/login')}>Login</button>
-                <button className="btn btn-outline-info border-3 fs-5" onClick={()=> navigate('/user')}>User</button>
-                <button className="btn btn-outline-info border-3 fs-5" onClick={()=> navigate('/favorites')}>Favorites</button>
-                <button className="btn btn-outline-info border-3 fs-5" onClick={()=> navigate('/carrito')}>🛒</button>
-                <button className="btn btn-outline-info border-3 fs-5" onClick={()=> navigate('/categorypage')}>category</button>
+                <button className="btn btn-outline-info border-2 fs-5" onClick={()=> navigate('/categorypage')}>category</button>
+                <button className="btn btn-outline-info border-2 fs-5" onClick={()=> navigate('/login')}>Login</button>
+                <button className="btn btn-outline-info border-2 fs-5" onClick={()=> navigate('/carrito')}>🛒</button>
+                <button className="btn btn-outline-info border-2 fs-5" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">User</button>
+            </div>
+            <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <div className={`offcanvas-header ${style.perfilCanvaHeader}`}>
+                    <h5 className="offcanvas-title text-info" id="offcanvasExampleLabel">Perfil name</h5>
+                    <button type="button" className="btn btn-outline-info fs-5" data-bs-dismiss="offcanvas" aria-label="Close">✖</button>
+                </div>
+                <div className={`offcanvas-body d-flex flex-column gap-3 ${style.perfilCanvaBody}`}>
+                    <button className="btn btn-outline-info border-3 fs-5" onClick={()=> navigate('/user')} data-bs-dismiss="offcanvas" aria-label="Close">Profile</button>
+                    <button className="btn btn-outline-info border-3 fs-5" onClick={()=> navigate('/favorites')} data-bs-dismiss="offcanvas" aria-label="Close">Favorites</button>
+                    <button className="btn btn-outline-info border-3 fs-5" onClick={()=> navigate('/')} data-bs-dismiss="offcanvas" aria-label="Close">logOut</button>
+                </div>
             </div>
         </nav>
     )
