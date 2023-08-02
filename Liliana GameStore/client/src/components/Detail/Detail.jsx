3f937a94887e84b522c0976d33579577a3dbf783
diego insaurralde <djinsaurralde38@gmail.com>
2023-08-02 19:29:24 -0300

    Detail + Comments

--- Liliana GameStore/client/src/components/Detail/Detail.jsx
+++ Liliana GameStore/client/src/components/Detail/Detail.jsx
@@ -2,13 +2,53 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductDetail, clearDetail } from "../../Redux/actions"
+import style from "./Detail.module.css"
+import DisqusComments from '../DisqusComments/DisqusComments'
 
 function Detail() {
 
     const { id } = useParams();
     const dispatch = useDispatch();
-    const detail = useSelector((state) => state.detail);    
+    const detail = useSelector((state) => state.detail);
+    
     const [quantity, setQuantity] = useState(1);
+
+    const fullStars = Math.floor(detail.rating);
+    const hasHalfStar = detail.rating - fullStars >= 0.5;
+    
+    const fullStarsElements = [];
+    for (let i = 0; i < fullStars; i++) {
+      fullStarsElements.push(<i key={i} className="bi bi-star-fill"></i>);
+    }
+    const halfStarElement = hasHalfStar ? (
+      <i key={fullStars} className="bi bi-star-half"></i>
+    ) : null;
+    const emptyStarsElements = [];
+    const totalStars = 5;
+    for (let i = 0; i < totalStars - fullStars - (hasHalfStar ? 1 : 0); i++) {
+      emptyStarsElements.push(<i key={fullStars + (hasHalfStar ? 1 : 0) + i} className="bi bi-star"></i>);
+    }
+
+    const handleIncrement = () => {
+      if (quantity < detail.stock) {
+        setQuantity(quantity + 1);
+      }
+    };
+    const handleDecrement = () => {
+      if (quantity > 1) {
+        setQuantity(quantity - 1);
+      }
+    };
+
+    const handleBuyNow = () => {
+      console.log('Comprar Ahora');
+    };
+    const handleAddItem = () => {
+      console.log('Agregar al carrito');
+    };
+    const handleFavorites = () => {
+      console.log('Favoritos');
+    };
 
     useEffect(() => {
         dispatch(getProductDetail(id));
@@ -23,37 +63,93 @@ function Detail() {
       : "EN STOCK ✅"
 
     return(
-        <section className="py-5">
-          <div className="container px-4 px-lg-5 my-5">
-            <div className="row gx-4 gx-lg-5 align-items-center">
-              <div className="col-md-6">
-                <img className="card-img-top mb-5 mb-md-0" src={detail.image} alt={detail.name} />
-              </div>
-              <div className="col-md-6">
-                <div className="small mb-1">{detail.rating} ⭐</div>
-                <h1 className="display-5 fw-bolder">{detail.name}</h1>
-                <div className="fs-5 mb-5">
-                  <span className="text-decoration-line-through">$?</span>
-                  <span>  ${detail.price}</span>
+        <section className={`py-5 ${style.vBackground}`}>
+          <div className="container">
+            <div className="row gx-5">
+              <aside className="col-lg-6">
+                <div className="border rounded-4 mb-3 d-flex justify-content-center">
+                  <a
+                    data-fslightbox="mygalley"
+                    className="rounded-4"
+                    target="_blank"
+                    data-type="image"
+                    href={detail.image}
+                  >
+                    <img
+                      style={{ maxWidth: '100%', maxHeight: '100vh', margin: 'auto' }}
+                      className="rounded-4 fit"
+                      src={detail.image}
+                      alt={detail.name}
+                    />
+                  </a>
                 </div>
-                <div className="fs-5 mb-5">
-                  <span>MARCA: </span>
+              </aside>
+              <main className="col-lg-6">
+                <div className="ps-lg-3">
+                  <h4 className={`title ${style["text-dark"]} ${style["green-text"]}`}>{detail.name}<br />{detail.category}</h4>
+                  <div className="d-flex flex-row my-3">
+                  <div className="text-warning mb-1 me-2">
+                    {fullStarsElements}
+                    {halfStarElement}
+                    {emptyStarsElements}
+                    <span className="ms-1">{detail.rating}</span>
+                  </div>
+                    <span className="text-success ms-2">{stockMessage}</span>
+                  </div>
+
+                  <div className="mb-3">
+                    <span className="h5">${detail.price}</span>
+                  </div>
+
+                  <p>{detail.description}</p>
+
+                  <div className="row">
+                    <dt className="col-3">Brand</dt>
+                    <dd className="col-9">{detail.brand}</dd>
+
+                    {/* <dt className="col-3">Type:</dt>
+                    <dd className="col-9">Regular</dd> */}
+
+                  </div>
+
+                  <hr />
+
+                  <div className="row mb-4">
+                    <div className="col-md-4 col-6 mb-3">
+                      <label className="mb-2 d-block">Cantidad</label>
+                      <div className="input-group mb-3" style={{ width: '170px' }}>
+                        <button className="btn btn-white border border-secondary px-3" type="button" id="button-addon1" data-mdb-ripple-color="dark" onClick={handleDecrement} >
+                          <i className="bi bi-dash"></i>
+                        </button>
+                        <input
+                          type="text"
+                          value={quantity}
+                          min="1"
+                          max={detail.stock}
+                          className="form-control text-center border border-secondary"
+                          aria-label="Example text with button addon"
+                          aria-describedby="button-addon1"
+                        />
+                        <button className="btn btn-white border border-secondary px-3" type="button" id="button-addon2" data-mdb-ripple-color="dark" onClick={handleIncrement} >
+                          <i className="bi bi-plus"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <a href="#" className="btn btn-warning shadow-0" onClick={handleBuyNow}>
+                    Comprar Ahora
+                  </a>
+                  <a href="#" className="btn btn-primary shadow-0" onClick={handleAddItem} >
+                    <i className="me-1"></i>🛒 Agregar al carrito
+                  </a>
+                  <a href="#" className="btn btn-light border border-secondary py-2 icon-hover px-3" onClick={handleFavorites} >
+                    <i className="me-1 fa fa-heart fa-lg"></i> Favoritos
+                  </a>
                 </div>
-                <div className="fs-5 mb-5">
-                  <span>{stockMessage}</span>
-                </div>
-                <p className="lead">{detail.description}</p>
-                <div className="d-flex">
-                  <input className="form-control text-center me-3" id="inputQuantity" type="number" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} min="1" max={detail.stock} style={{ maxWidth: '3rem' }} />
-                  <button className="btn btn-outline-dark flex-shrink-0" type="button">
-                    <i className="bi-cart-fill me-1"></i>Comprar Ahora</button>
-                  <button className="btn btn-outline-dark flex-shrink-0" type="button">
-                    <i className="bi-cart-fill me-1"></i>Agregar al carrito</button>
-                  <button>FAV</button>
-                </div>
-              </div>
+              </main>
             </div>
           </div>
+          <DisqusComments/>
         </section>
     )
 }
---
+++ Liliana GameStore/client/src/components/DisqusComments/DisqusComments.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import style from "./DisqusComments.module.css"
+
+const DisqusComments = () => {
+  useEffect(() => {
+    const loadDisqusScript = () => {
+      const d = document;
+      const s = d.createElement('script');
+      s.src = 'https://liliana-gamestore.disqus.com/embed.js';
+      s.setAttribute('data-timestamp', +new Date());
+      (d.head || d.body).appendChild(s);
+    };
+    loadDisqusScript();
+    return () => {
+      const d = document;
+      const disqusScript = d.getElementById('disqus_thread');
+      if (disqusScript) {
+        disqusScript.remove();
+      }
+    };
+  }, []);
+  return (
+    <div className={style.disqusSize}>
+      <div id="disqus_thread"></div>
+      <noscript>
+        Please enable JavaScript to view the{' '}
+        <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
+      </noscript>
+    </div>
+  );
+};
+
+export default DisqusComments;
