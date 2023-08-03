c684219e489b79c9eb3110b7f33fe21bf16bfaa0
Nicolas Rojas <nico_rojas09@hotmail.com>
2023-08-03 19:58:22 -0300

    login funcionando

--- Liliana GameStore/client/src/components/Login/Login.jsx
+++ Liliana GameStore/client/src/components/Login/Login.jsx
@@ -1,29 +1,35 @@
 import { useNavigate } from "react-router-dom";
 import style from "./Login.module.css"
 import axios from "axios"
-import { useEffect } from "react";
+import { useState } from "react";
 
 const Login = () => {
     const navigate = useNavigate()
-    const userData = {
-        username: 'Pirulo1',
-        password: '12345678'
-      };
+    const [userData, setUserData] = useState({
+        username: '',
+        password: ''
+    })
 
-    useEffect(async()=>{
+    const handleChange = (event) => {
+        setUserData({
+            ...userData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
         try {
             let respuesta = await axios.post('http://localhost:3001/LilianaGameStore/login', userData)
             .then(response => {
                 // Manejo de la respuesta exitosa
                 console.log(response.data);
-              })                                                                      
+                localStorage.setItem("user", response.data.username)
+                navigate('/')
+              })                                                                     
         } catch (error) {
             console.log(error.response.data);
         }
-    },[])
-
-    const handleSubmit = () => {
-        
     }
 
     return(
@@ -33,11 +39,11 @@ const Login = () => {
                     <form onSubmit={handleSubmit}>
                         <div className="mb-3">
                             <label for="exampleInputEmail1" className="form-label">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                            <input type="text" className="form-control" name="username" value={userData.username} onChange={handleChange} id="exampleInputEmail1" aria-describedby="emailHelp"/>
                         </div>
                         <div className="mb-3">
                             <label for="exampleInputPassword1" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1"/>
+                            <input type="password" className="form-control" name="password" value={userData.password} onChange={handleChange} id="exampleInputPassword1"/>
                         </div>
                         <button type="submit" className="btn btn-outline-info">Submit</button>
                     </form>
