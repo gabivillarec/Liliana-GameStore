56b5f5c9935076d31cf23d3dd8b5f38fa264a005
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-08-04 17:09:03 -0300

    gabiVillalobos: Elimine el modelo PurchaseHistory y todo a lo que se relaciona , termine las ordenes , 'CONTROLADORES FINALIZADOS' por ahora'

--- Liliana GameStore/api/src/routes/index.js
+++ Liliana GameStore/api/src/routes/index.js
@@ -7,7 +7,6 @@ const brandRoutes = require('./brandRoutes');
 const cartRoutes = require('./cartRoutes');
 const categoryRoutes = require('./categoryRoutes');
 const orderRoutes = require('./orderRoutes');
-const purchaseHistoryRoutes = require('./purchaseHistoryRoutes');
 const socketRoutes = require('./socketRoutes');
 const subCategoryRoutes = require('./subCategoryRoutes');
 
@@ -15,17 +14,16 @@ const subCategoryRoutes = require('./subCategoryRoutes');
 const router = Router();
 
 // Configurar los routers
-router.use('/LilianaGameStore',usersRoutes);
-//router.use('/LilianaGameStore',login);
 router.use('/LilianaGameStore',productRoutes);
 router.use('/LilianaGameStore',brandRoutes);
 router.use('/LilianaGameStore',cartRoutes);
 router.use('/LilianaGameStore',categoryRoutes);
 router.use('/LilianaGameStore',favoritesRoutes);
 router.use('/LilianaGameStore',orderRoutes);
-//router.use('/LilianaGameStore',purchaseHistoryRoutes);
 router.use('/LilianaGameStore',socketRoutes);
 router.use('/LilianaGameStore',subCategoryRoutes);
+router.use('/LilianaGameStore',usersRoutes);
+router.use('/LilianaGameStore',login);
 
 
 module.exports = router;
--- Liliana GameStore/api/src/routes/orderRoutes.js
+++ Liliana GameStore/api/src/routes/orderRoutes.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 const createOrder = require('../Controllers/createOrder');
 const getOrders = require ('../Controllers/getOrders')
+const deleteOrder = require('../Controllers/deleteOrder')
 
 const orderRoutes = Router();
 
 orderRoutes.post('/order', createOrder);
 
 orderRoutes.get('/order', getOrders );
+
+orderRoutes.delete('/order/:order_numer', deleteOrder);
 
 
 module.exports = orderRoutes;
