81f0b7dfbee90ee8e0796be956a896e44fb4e73e
Nicolas Rojas <nico_rojas09@hotmail.com>
2023-08-13 17:34:00 -0300

    login con ban

--- Liliana GameStore/api/src/Controllers/login.js
+++ Liliana GameStore/api/src/Controllers/login.js
@@ -5,7 +5,7 @@ const util = require('util');
 const login = async (req, res) => {
 
     try {
-        const { username, password, email } = req.body;
+        const { username, password } = req.body;
 
         if(username){ 
             const user = await Users.findOne({ where: { username } })
@@ -15,22 +15,11 @@ const login = async (req, res) => {
         
                 // Comparo el password con el hash almacenado
                 const passwordMatches = await bcrypt.compare(password, user.password);
-    
+                if(user.disabled){
+                    return res.status(400).json({ error: 'Usuario deshabilitado' });
+                }
                 return passwordMatches 
                 ? res.json(user)       
-                : res.status(400).json({ error: 'Invalid Password' });
-            };
-        };
-
-        if(email){ 
-            const emailUser = await Users.findOne( { where: { email } })
-            if (emailUser){ 
-                await util.promisify(setTimeout)(100);
-    
-                const passwordMatches = await bcrypt.compare(password, emailUser.password);
-        
-                return passwordMatches 
-                ? res.json(emailUser)       
                 : res.status(400).json({ error: 'Invalid Password' });
             };
         };
