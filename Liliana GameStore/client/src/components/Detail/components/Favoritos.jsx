8bc9c0a3dbf9341231d07c06cc6e227a848e0133
Galbfran <francogalbiati984@gmail.com>
2023-08-17 22:18:58 -0300

    modulos detail

---
+++ Liliana GameStore/client/src/components/Detail/components/Favoritos.jsx
@@ -0,0 +1,16 @@
+import { useState , useEffect } from "react"
+
+
+
+const Favoritos = ({favorito}) =>{
+    useEffect(() =>{
+    },[favorito])
+
+    return(
+        <button href="#" className={`btn ${favorito.style} border border-secondary py-2 icon-hover px-3`} onClick={favorito.handler} >
+            <i className="me-1 fa fa-heart fa-lg"></i> Favoritos
+        </button>
+    )
+}
+
+export default Favoritos
